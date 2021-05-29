eb313dd224d0b48b4bcf7d9d2323ade16e39c371
Warrinot <warrinot@gmail.com>
2021-05-29 09:08:48 +0300

    fixed url for API specs

--- digital_calls/urls.py
+++ digital_calls/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -24,10 +24,9 @@ urlpatterns = [
     path('api/', include('calls.urls')),
 
     # API docs
-    path(r'swagger(<format>\.json|\.yaml)',
-         schema_view.without_ui(cache_timeout=0),
-         name='schema-json'
-         ),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='schema-json'),
     path(r'swagger/', schema_view.with_ui('swagger',
                                           cache_timeout=0
                                           ),
